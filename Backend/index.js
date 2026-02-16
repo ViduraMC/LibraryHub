ef7f348f2010b0e58a26f3de5c39a9be1f087a65
Lakkhana <lakkhanam05@gmail.com>
2026-02-16 14:53:25 +0530

    basic project struct

---
+++ Backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import connectDB from "./config/mongodb.js"
+import "dotenv/config";
+import cors from "cors";
+
+
+const app= express();
+
+connectDB();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req,res)=>{
+  res.send("API working");
+});
+
+
+const PORT= process.env.PORT || 3000;
+app.listen(PORT, ()=> {
+  console.log(`Server running on port: ${PORT}`);
+});
