ffa9a1367cbe09fd4f4d101cfde46851228a5356
Lakkhana <lakkhanam05@gmail.com>
2026-02-20 15:09:43 +0530

    added updateComputer in computer controller

--- Backend/controller/computer.controller.js
+++ Backend/controller/computer.controller.js
@@ -34,4 +34,24 @@ export const getAllComputers = async (req, res)=>{
   } catch (error) {
     return res.status(500).json({error: "Unable to fetch all computers", error});
   }
-}
+};
+
+export const updateComputer = async(req, res)=> {
+  try {
+    const {id}= req.params;
+    const {computerNumber, status}= req.body;
+
+    const updatedPC= await Computer.findByIdAndUpdate(
+      id,
+      {computerNumber, status},
+      {new: true, runValidators: true}
+    );
+
+    if(!updatedPC) return res.status(404).json({message: "Computer not found!"});
+
+    return res.status(200).json({message:"Update successful", data: updatedPC});
+
+  } catch (error) {
+    return res.status(400).json({error: "Unable to update computers", error});
+  }
+};
